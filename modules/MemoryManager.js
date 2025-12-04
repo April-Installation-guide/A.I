06fc32e4ac1edb23b02b1041c263e1630ef5b386
April-Installation-guide <sauljalinas1@gmail.com>
2025-12-04 04:47:25 +0100

    MemoryManager.js

--- modules/MemoryManager.js
+++ modules/MemoryManager.js
@@ -1,1 +1,47 @@
+export class MemoryManager {
+    constructor(maxHistory = 270) {
+        this.conversationMemory = new Map();
+        this.MAX_HISTORY = maxHistory;
+    }
 
+    obtenerHistorialUsuario(userId) {
+        if (!this.conversationMemory.has(userId)) {
+            this.conversationMemory.set(userId, []);
+        }
+        return this.conversationMemory.get(userId);
+    }
+
+    agregarAlHistorial(userId, rol, contenido) {
+        const historial = this.obtenerHistorialUsuario(userId);
+        historial.push({ rol, contenido, timestamp: Date.now() });
+        
+        if (historial.length > this.MAX_HISTORY) {
+            historial.splice(0, historial.length - this.MAX_HISTORY);
+        }
+    }
+
+    limpiarMemoriaAntigua(dias = 7) {
+        const ahora = Date.now();
+        const msPorDia = 24 * 60 * 60 * 1000;
+        
+        for (const [userId, historial] of this.conversationMemory.entries()) {
+            const historialReciente = historial.filter(
+                msg => (ahora - msg.timestamp) < (dias * msPorDia)
+            );
+            
+            if (historialReciente.length === 0) {
+                this.conversationMemory.delete(userId);
+            } else {
+                this.conversationMemory.set(userId, historialReciente);
+            }
+        }
+    }
+
+    obtenerEstadisticas() {
+        const totalUsuarios = this.conversationMemory.size;
+        const totalMensajes = Array.from(this.conversationMemory.values())
+            .reduce((sum, hist) => sum + hist.length, 0);
+        
+        return { totalUsuarios, totalMensajes };
+    }
+}
