09a824fdeb5dc04ade046edfb2c1b673cebcb58b
April-Installation-guide <sauljalinas1@gmail.com>
2025-12-21 19:46:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 11000;
 
 // ===============================
 // Middleware
